5db2e31debc35c2b4314b7045936ef697307c527
lf <1521371538@qq.com>
2026-07-06 20:46:21 +0800

    修订版，学生端+教师端分离

--- backend/apps/recognition/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class RecognitionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.recognition'
-    verbose_name = '人脸识别对接'
--- backend/apps/recognition/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.face_register, name='face-register'),
-    path('verify/', views.face_verify, name='face-verify'),
-    path('identify/', views.face_identify, name='face-identify'),
-    path('health/', views.face_service_health, name='face-health'),
-]
